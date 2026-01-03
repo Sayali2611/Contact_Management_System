e56fb4513bfcac5673a9e167eff60e1d31d64791
Sayali2611 <sayalibhanage2005@gmail.com>
2026-01-03 16:06:09 +0530

    Update API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/contacts';
+const API_URL = 'https://contact-app-backend.onrender.com/api/contacts';
 
 export const getAllContacts = async () => {
   try {
